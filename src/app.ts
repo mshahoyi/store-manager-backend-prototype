bb98bdb7fb91dc27ded40f7a486a434804baf768
Mohammed Shahoyi <muyabb@gmail.com>
2021-05-22 20:53:58 +0300

    adds cors

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,25 @@
-import express, { Request } from "express";
-import storeRoute from "./routes/storeRoutes";
-import globalErrorHandler from "./utils/globalErrorHandler";
-import { paginationQueryMiddleware } from "./utils/paginationUtils";
-import morgan from "morgan";
-import categoryRoute from "./routes/categoryRoutes";
-import productRoute from "./routes/productRoutes";
+import express, { Request } from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import storeRoute from './routes/storeRoutes';
+import globalErrorHandler from './utils/globalErrorHandler';
+import { paginationQueryMiddleware } from './utils/paginationUtils';
+import categoryRoute from './routes/categoryRoutes';
+import productRoute from './routes/productRoutes';
 
 const app = express();
 
 // Middlewares
 app.use(express.json());
-app.use(morgan(process.env.NODE_ENV === "production" ? "tiny" : "dev"));
+app.use(cors());
+app.use(morgan(process.env.NODE_ENV === 'production' ? 'tiny' : 'dev'));
 app.use(paginationQueryMiddleware);
 
 // Routers
-app.use(express.static("public"));
-app.use("/api/v1/stores", storeRoute);
-app.use("/api/v1/categories", categoryRoute);
-app.use("/api/v1/products", productRoute);
+app.use(express.static('public'));
+app.use('/api/v1/stores', storeRoute);
+app.use('/api/v1/categories', categoryRoute);
+app.use('/api/v1/products', productRoute);
 
 // Global Error Handler
 app.use(globalErrorHandler);
